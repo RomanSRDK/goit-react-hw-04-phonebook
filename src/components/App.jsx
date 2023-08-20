85884452cf32fe22d20196c085e6b389bf512d12
Roman Serdiuk <roman.srdk21@gmail.com>
2023-08-20 14:37:54 +0300

    commit
    LocalStorage не работает

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,82 +1,63 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from "./ContactLIst/ContactList";
 import Filter from "./Filter/Filter";
 import Title from "./Title/Title";
 import styles from './App.module.css'
 import { nanoid } from 'nanoid';
+import { useLocalStorage } from 'CustomHooks/UseLocalStorage';
 
-export default class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+export default function App() {
+  // useLocalStorage();
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+
+  const addContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const contactCheck = contacts.find(({ name }) => name === newContact.name);
+
+    if (contactCheck !== undefined) {
+      alert(`${newContact.name} is already in our contacts book`);
+      return;
+    }
+    setContacts(prevState => [newContact, ...prevState]);
   };
 
-  componentDidMount() {
-    const getContacts = localStorage.getItem('contacts');
-    const parseContacts = JSON.parse(getContacts);
-    if (parseContacts) {
-      this.setState({ contacts: parseContacts });
-    }
-  }
+  const onChange = event => {
+    setFilter(event.target.value);
+  };
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  const onButtonDelete = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  };
 
-  addContact = newContact => {
-  const contactsCheck = this.state.contacts.some(
-    ({ name }) => name === newContact.name,
+  const loweredContacts = filter.toLowerCase();
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(loweredContacts)
   );
-  if (contactsCheck ) {
-    alert(`${newContact.name} is already in contacts book`);
-    return;
-  }
-    this.setState(prevState => ({
-      contacts: [{ ...newContact, id:nanoid()}, ...prevState.contacts],
-  }));
-};
-
-  onChange = event => {
-   const {name,value}=event.currentTarget
-    this.setState({
-      [name]:value
-    });
-  };
-
-onButtonDelete =  id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id),
-    }));
-  this.reset();
-};
-
-  reset = () => {
-    this.setState({ filter: '' });
-  };
-
-render() {
-  const { filter, contacts } = this.state;
-  const loweredContacts = filter.toLowerCase().trim();
-  const filteredContacts = contacts.filter(({ name }) => name.toLowerCase().includes(loweredContacts));
 
   return (
     <>
-    <section className={styles.section}>
-      <Title text="PhoneBook" />
-      <ContactForm addContact={this.addContact} list={this.state.contacts} />
-      <Title text="Contacts" />
-        <Filter value={this.state.filter} onChange={this.onChange} />
-      <ContactList filtered={filteredContacts} onButtonDelete={this.onButtonDelete}/>
+      <section className={styles.section}>
+        <Title text="PhoneBook" />
+        <ContactForm addContact={addContact} list={contacts} />
+        <Title text="Contacts" />
+        <Filter value={filter} onChange={onChange} />
+        <ContactList
+          filtered={filteredContacts}
+          onButtonDelete={onButtonDelete}
+        />
       </section>
     </>
-  )
+  );
 }
-};
